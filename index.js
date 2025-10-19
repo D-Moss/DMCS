05aa4c43711dc81fac5c658592ab7134d564fa16
Diona Moss <123892824+D-Moss@users.noreply.github.com>
2025-10-18 20:38:03 -0400

    Add files via upload
    Added an action and JS to make the form active

--- index.js
+++ index.js
@@ -8,15 +8,35 @@ const totalCards = document.querySelectorAll('.experience-card').length;
 const maxIndex = Math.ceil(totalCards / 2) - 1;
 
 nextBtn.addEventListener('click', () => {
-  if (currentIndex < maxIndex) {
-    currentIndex++;
-    track.style.transform = `translateX(-${currentIndex * cardWidth}px)`;
-  }
+	if (currentIndex < maxIndex) {
+		currentIndex++;
+		track.style.transform = `translateX(-${currentIndex * cardWidth}px)`;
+	}
 });
 
 prevBtn.addEventListener('click', () => {
-  if (currentIndex > 0) {
-    currentIndex--;
-    track.style.transform = `translateX(-${currentIndex * cardWidth}px)`;
-  }
+	if (currentIndex > 0) {
+		currentIndex--;
+		track.style.transform = `translateX(-${currentIndex * cardWidth}px)`;
+	}
+});
+
+//FORM CONTACT SECTION
+const form = document.querySelector("form");
+const success = document.getElementById("form-success");
+form.addEventListener("submit", async (e) => {
+	e.preventDefault();
+	const data = new FormData(form);
+	const response = await fetch(form.action, {
+		method: "POST",
+		body: data,
+		headers: { Accept: "application/json" },
+	});
+
+	if (response.ok) {
+		form.reset();
+		success.style.display = "block";
+	} else {
+		alert("Oops! Something went wrong.");
+	}
 });
